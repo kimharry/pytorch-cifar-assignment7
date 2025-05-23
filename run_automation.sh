42f7e0792018ecd79ed943107cf34131e026f90b
kimharry <kimharry0610@gmail.com>
2025-05-23 12:34:51 +0000

    해치웠나

--- run_automation.sh
+++ run_automation.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
-python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean_std --weight_init kaiming &
+python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean_std --weight_init xavier &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing per_channel_mean_std --weight_init xavier &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing per_channel_mean_std --weight_init xavier &
 
+wait
+
+python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean_std --weight_init kaiming &
 python main.py --num_epochs 100 --activation tanh --data_preprocessing per_channel_mean_std --weight_init kaiming &
 python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing per_channel_mean_std --weight_init kaiming &
 
 wait
 
-python main.py --num_epochs 100 --activation relu --data_preprocessing mean_img --weight_init kaiming &
-python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean --weight_init kaiming &
+python main.py --num_epochs 100 --activation relu --data_preprocessing mean_img --weight_init gaussian &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing mean_img --weight_init gaussian &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing mean_img --weight_init gaussian &
 
 wait
 
-python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean_std --weight_init gaussian &
-python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean_std --weight_init xavier &
+python main.py --num_epochs 100 --activation relu --data_preprocessing mean_img --weight_init xavier &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing mean_img --weight_init xavier &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing mean_img --weight_init xavier &
+
+wait
+
+python main.py --num_epochs 100 --activation relu --data_preprocessing mean_img --weight_init kaiming &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing mean_img --weight_init kaiming &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing mean_img --weight_init kaiming &
+
+wait
+
+python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean --weight_init gaussian &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing per_channel_mean --weight_init gaussian &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing per_channel_mean --weight_init gaussian &
+
+wait
+
+python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean --weight_init xavier &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing per_channel_mean --weight_init xavier &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing per_channel_mean --weight_init xavier &
+
+wait
+
+python main.py --num_epochs 100 --activation relu --data_preprocessing per_channel_mean --weight_init kaiming &
+python main.py --num_epochs 100 --activation tanh --data_preprocessing per_channel_mean --weight_init kaiming &
+python main.py --num_epochs 100 --activation leaky_relu --data_preprocessing per_channel_mean --weight_init kaiming &
 
 wait
